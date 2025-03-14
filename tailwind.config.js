4bd23f7cf229270dfa36601f94d06f39903b28c8
Kevin Barrios <kusakus@live.com>
2025-03-13 19:48:56 -0700

    updated tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  content: ["./**/*.html"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./comps/**/*.html",
+    "./projects/**/*.html",
+    "./contact/**/*.html"
+  ],
   theme: {
-    extend: {},
   },
   plugins: [],
-};
-
+}
